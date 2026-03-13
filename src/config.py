c2394920cffdf71667026d40f75f5316bd33fdb3
Hoang Ngoc <hoangngoc3691@gmail.com>
2026-03-13 22:40:09 +0700

    feature: config path for load dataset

---
+++ src/config.py
@@ -0,0 +1,21 @@
+
+# Relate to path
+PATH_FOLDER_RAW = "../data/raw"
+PATH_FOLDER_IMAGE_RAW = PATH_FOLDER_RAW + "image"
+PATH_FOLDER_TABLE_RAW = PATH_FOLDER_RAW + "table"
+
+
+# Relate to image
+CLASS_NAMES = ['dog', 'spider', 'cat', 'sheep', 'elephant', 'chicken', 'butterfly', 'cow', 'squirrel', 'horse']
+CLASS_INDEX = {
+    'dog': 1,
+    'spider': 2,
+    'cat': 3,
+    'sheep': 4,
+    'elephant': 5,
+    'chicken': 6,
+    'butterfly':7,
+    'cow': 8,
+    'squirrel': 9,
+    'horse': 10,
+}
