f4b7392a347cfb7c6de51a91e9e4f77a27c488c2
zeepaulus <duybao.zp@gmail.com>
2026-03-18 18:29:03 +0700

    feature: add pixel distribution logic

--- src/image/analysis_pixel_distribution.py
+++ src/image/analysis_pixel_distribution.py
@@ -4,6 +4,8 @@ from core.data_base import Object
 from image.dataset import ImageDataset
 from visualization.distribution import plot_histogram, plot_kde
 
+# Task 7
+
 class Distribution(Visualization):
     def __init__(self):
         self.step_name = "Base Analysis"
@@ -14,15 +16,16 @@ class Distribution(Visualization):
         print(f"Bước xử lý : {self.step_name}")
         print(f"Tập dữ liệu: {self.dataset_name}")
         print(f"Trạng thái : {self.status}")
-        print("="*50)
 
+    # chỗ này tui có làm 1 biến max_samples để giới hạn số lượng pixel lấy ra do kde xử lý input lớn bị lâu
+    # ông thấy không cần thì bỏ cũng được
     def _extract_pixels(self, obj: ImageDataset, max_samples=200000):
         """ Task 7: Lấy thông tin pixel, giữ cấu trúc 3 kênh màu và áp dụng Sampling """
         images, _ = obj.images
         if len(images) == 0:
             raise ValueError("Dataset rỗng.")
             
-        # Biến đổi mảng list(N, H, W, 3) thành 1 mảng numpy 2D (Tổng pixel, 3)
+        # Biến đổi mảng list(N, H, W, 3) thành 1 mảng numpy có shape (Tổng pixel, 3); 3 là số kênh màu nha
         pixels = np.concatenate([img.reshape(-1, 3) for img in images])
 
         if max_samples is not None and len(pixels) > max_samples:
@@ -35,7 +38,7 @@ class Distribution(Visualization):
 class HistogramDistribution(Distribution):
     def __init__(self):
         super().__init__()
-        self.step_name = "Analysis Histogram Distribution"
+        self.step_name = "Histogram Distribution"
 
     def run(self, obj: Object):
         if isinstance(obj, ImageDataset):
@@ -60,7 +63,7 @@ class HistogramDistribution(Distribution):
 class KDEDistribution(Distribution):
     def __init__(self):
         super().__init__()
-        self.step_name = "Analysis KDE Distribution"
+        self.step_name = "KDE Distribution"
 
     def run(self, obj: Object):
         if isinstance(obj, ImageDataset):
