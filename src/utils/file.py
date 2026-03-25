36f45660d74f1555b5492f58784c2eda27cbb6d2
duyhoang <hoangduymoshi777@gmail.com>
2026-03-25 12:52:16 +0700

    feature: fix type of X variable from numpy to list in load_images function

--- src/utils/file.py
+++ src/utils/file.py
@@ -3,27 +3,33 @@ from pathlib import Path
 import cv2
 import numpy as np
 
-def load_images(path: str, image_size = (128,128)):
+def load_images(path: str, image_size=None):
     root = Path(path)
     class_names = config.CLASS_NAMES
     class_to_idx = config.CLASS_INDEX
 
-    X,Y, file_names ,paths = [],[],[],[]
-
+    X, Y, file_names, paths = [], [], [], []
     exts = config.IMAGE_EXTS
+    
+    printed_original_size = False
+
     for class_name in class_names:
-        count  = 0
+        count = 0
         class_dir = root / class_name
         if not class_dir.exists():
             continue
 
         for item in class_dir.rglob("*"):
-            if item.suffix.lower() not in exts :
+            if item.suffix.lower() not in exts:
                 continue
 
             img = cv2.imread(str(item))
             if img is None:
                 continue
+            if not printed_original_size:
+                height, width, channels = img.shape
+                # print(f"Kích thước ảnh gốc (Mẫu đầu tiên): {width}x{height} pixels, {channels} kênh màu")
+                printed_original_size = True
 
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # img = cv2.resize(img, image_size, interpolation=cv2.INTER_AREA)
@@ -34,11 +40,11 @@ def load_images(path: str, image_size = (128,128)):
             file_names.append(str(file_name))
             paths.append(str(item))
 
-            count+=1
+            count += 1
 
-    X = np.array(X, dtype=np.uint8)
     Y = np.array(Y, dtype=np.int64)
-    return X, Y, paths, class_to_idx, file_names
+    
+    return X, Y, class_to_idx, paths, file_names
 
 def load_table(path: str):
     return
